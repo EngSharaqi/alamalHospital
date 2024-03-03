d87031991e436d0ea35f4218e4eaaa5d94b6ab40
EngSharaqi <muhammadsharaqi@gmail.com>
2024-03-03 20:15:23 +0200

    arabic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,10 @@
 'use strict';
 
-
-
-/**
- * add event listener on multiple elements
- */
-
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
   }
 }
-
-
-
-/**
- * PRELOADER
- * 
- * preloader will be visible until document load
- */
 
 const preloader = document.querySelector("[data-preloader]");
 
@@ -28,13 +14,6 @@ window.addEventListener("load", function () {
 });
 
 
-
-/**
- * MOBILE NAVBAR
- * 
- * show the mobile navbar when click menu button
- * and hidden after click menu close button or overlay
- */
 
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
@@ -47,14 +26,6 @@ const toggleNav = function () {
 }
 
 addEventOnElements(navTogglers, "click", toggleNav);
-
-
-
-/**
- * HEADER & BACK TOP BTN
- * 
- * active header & back top btn when window scroll down to 100px
- */
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
@@ -70,12 +41,6 @@ const activeElementOnScroll = function () {
 }
 
 window.addEventListener("scroll", activeElementOnScroll);
-
-
-
-/**
- * SCROLL REVEAL
- */
 
 const revealElements = document.querySelectorAll("[data-reveal]");
 
